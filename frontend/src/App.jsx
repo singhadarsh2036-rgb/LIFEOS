bac9178a756934143c1252436e9324046e3a56c3
Adarsh Singh <singhadarsh2036@gmail.com>
2026-09-23 10:14:16 +0530

    Fix login persistence on app reopen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,48 +15,66 @@ import Trips from './Trips.jsx'
 import './App.css'
 
 function App() {
+  const token = localStorage.getItem('token')
+
   return (
     <BrowserRouter>
       <Routes>
 
+        {/* Home */}
         <Route
           path="/"
-          element={<Navigate to="/login" replace />}
+          element={
+            token
+              ? <Navigate to="/dashboard" replace />
+              : <Navigate to="/login" replace />
+          }
         />
 
+        {/* Login */}
         <Route
           path="/login"
-          element={<Login />}
+          element={
+            token
+              ? <Navigate to="/dashboard" replace />
+              : <Login />
+          }
         />
 
+        {/* Register */}
         <Route
           path="/register"
           element={<Register />}
         />
 
+        {/* Dashboard */}
         <Route
           path="/dashboard"
           element={<Dashboard />}
         />
 
+        {/* Tasks */}
         <Route
           path="/tasks"
           element={<Tasks />}
         />
 
+        {/* Reminders */}
         <Route
           path="/reminders"
           element={<Reminders />}
         />
 
+        {/* Trips */}
         <Route
           path="/trips"
           element={<Trips />}
         />
 
+        {/* Unknown route */}
         <Route
           path="*"
-          element={<Navigate to="/login" replace />}
+          element={<Navigate to="/" replace />}
         />
 
       </Routes>
